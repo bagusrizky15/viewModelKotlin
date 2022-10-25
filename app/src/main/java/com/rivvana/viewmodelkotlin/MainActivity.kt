b9b6fb9e009197d1bc212cf7ee94efc8088c1e46
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-10-25 20:18:25 +0700

    .

--- app/src/main/java/com/rivvana/viewmodelkotlin/MainActivity.kt
+++ app/src/main/java/com/rivvana/viewmodelkotlin/MainActivity.kt
@@ -2,8 +2,11 @@ package com.rivvana.viewmodelkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rivvana.viewmodelkotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/rivvana/viewmodelkotlin/MainViewModel.kt
@@ -0,0 +1,5 @@
+package com.rivvana.viewmodelkotlin
+
+class MainViewModel {
+
+}

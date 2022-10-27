d3dd7c75edc948bb241d9a328309d3657b86eeda
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-10-27 20:02:48 +0700

    .

--- app/src/main/java/com/rivvana/viewmodelkotlin/MainViewModel.kt
+++ app/src/main/java/com/rivvana/viewmodelkotlin/MainViewModel.kt
@@ -5,7 +5,9 @@ import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
     var counter : MutableLiveData<Int> = MutableLiveData(0)
+
     fun increment(angkaPertama: String, angkaKedua: String){
         counter.postValue(angkaPertama.toInt()+angkaKedua.toInt())
     }
+
 }
